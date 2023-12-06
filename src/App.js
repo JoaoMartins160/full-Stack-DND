b8af799b53dd411f04f9eb0345e502feb01559a7
Caio Tacahashi <caioeike14@gmail.com>
2023-12-05 22:23:20 -0300

    Feature / Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Sidebar from "./components/Sidebar";
 import Classes from "./pages/Classes";
 import Equipamentos from "./pages/Equipamentos";
@@ -8,10 +8,13 @@ import Racas from "./pages/Racas";
 import Regras from "./pages/Regras";
 import Home from "./pages/Home";
 import useWebSocket from "./websocket";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Login from "./pages/Login"
 
 function App() {
   const socket = useWebSocket("ws://localhost:8080");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (socket) {
@@ -21,18 +24,29 @@ function App() {
     }
   }, [socket]);
 
+  const handleLogin = (username, password) => {
+    // Aqui você pode adicionar a lógica para verificar o nome de usuário e a senha
+    // Por exemplo, você pode fazer uma solicitação à sua API para verificar as credenciais
+    // Neste exemplo, vamos assumir que o login é sempre bem-sucedido
+    setIsLoggedIn(true);
+    navigate('/');
+  };
+
   return (
     <>
-      <Sidebar></Sidebar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Classes" element={<Classes />} />
-        <Route path="/Equipamentos" element={<Equipamentos />} />
-        <Route path="/Habilidades" element={<Habilidades />} />
-        <Route path="/Monstros" element={<Monstros />} />
-        <Route path="/Raças" element={<Racas />} />
-        <Route path="/Regras" element={<Regras />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/login" element={isLoggedIn ? <Home /> : <Login onLogin={handleLogin} />} />
+          <Route path="/*" element={isLoggedIn ? <Sidebar /> : <Login onLogin={handleLogin} />} />
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Classes" element={isLoggedIn ? <Classes /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Equipamentos" element={isLoggedIn ? <Equipamentos /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Habilidades" element={isLoggedIn ? <Habilidades /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Monstros" element={isLoggedIn ? <Monstros /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Raças" element={isLoggedIn ? <Racas /> : <Login onLogin={handleLogin} />} />
+          <Route path="/Regras" element={isLoggedIn ? <Regras /> : <Login onLogin={handleLogin} />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    // Adicione sua lógica de autenticação aqui
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
